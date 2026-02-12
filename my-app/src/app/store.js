4f6b1d12ce3f9777c557341045eb2c711c0c1313
amjathstack <mohammedamjath772@gmail.com>
2026-02-12 13:50:00 +0530

    redux fix

--- my-app/src/app/store.js
+++ my-app/src/app/store.js
@@ -2,15 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import componentsReducer from '../features/components/componentsSlice';
 import cartReducer from '../features/cart/cartSlice';
 import productsReducer from '../features/products/productSlice';
-import addressReducer from '../../src/features/address/addressSlice';
-import orderReducer from '../../src/features/order/orderSlice';
 
 export const store = configureStore({
     reducer: {
         components: componentsReducer,
         cart: cartReducer,
         products: productsReducer,
-        address: addressReducer,
-        orders: orderReducer,
     },
 });
